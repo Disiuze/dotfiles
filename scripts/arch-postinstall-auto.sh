9a67daefcc95edc61fd34355bc4f44836b0c7c2e
Rafael <rkvramawadh@gmail.com>
2019-09-10 05:54:56 +0200

    finalize postinstall for now hopefully

--- scripts/arch-postinstall-auto.sh
+++ scripts/arch-postinstall-auto.sh
@@ -126,5 +126,7 @@ echo 'set softwrap' >> /home/$UNAME/.nanorc
 if [ $saku = 'y' ]; then
 	wget 'https://raw.githubusercontent.com/Disiuze/dotfiles/master/.config/sakura/sakura.conf' -O '/home/$UNAME/.config/sakura/sakura.conf'
 
+chown -R $UNAME:wheel /home/$UNAME
+
 echo "Post-Install complete!"
 echo "Reboot ASAP to ensure the system still works."
